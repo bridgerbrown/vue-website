99f1105290e0f08952d1929af2fbf8d6d09f6ea1
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-05-17 18:08:52 -0700

    dev error

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <nav 
+        class="w-full py-6 px-4"
+        :class="[`navbar-${theme}`, `bg-${theme}`]"
+    >
+        <div class="flex justify-between items-center">
+        <a href="#"
+            class="text-2xl font-semibold"
+        >
+            Vue.js Website
+        </a>
+        <div class="flex space-x-6">
+            <ul class="flex space-x-6">
+            <li v-for="(page, index) in pages" :key="index">
+                <a 
+                class="cursor-pointer" 
+                :class="{active: activePage == index}"
+                aria-current="page"
+                :href="page.link.url"
+                :title="`This link goes to the ${page.link.text} page`"
+                @click.prevent="navLinkClick(index)"
+                >
+                    {{ page.link.text }}
+                </a>
+            </li>
+            </ul>
+            <form action=""
+            class="flex"
+            >
+            <button
+                class="w-8 h-8 rounded-full bg-stone-300"
+                @click.prevent="changeTheme()"
+            >
+                Mode
+            </button>
+            </form>
+        </div>
+        </div>
+    </nav>
+</template>
+
+<script lang="js">
+    export default {
+        props: ['pages', 'activePage', 'theme', 'navLinkClick'],
+        data() {
+          return {
+            theme: 'light',
+          }
+        },
+        methods: {
+          changeTheme() {
+            let theme = 'light';
+
+            if (this.theme == 'light') {
+              theme = 'dark';
+            }
+
+            this.theme = theme;
+          }
+        }
+    }
+</script>
---
+++ src/components/PageViewer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mt-8 mx-8">
+        <h1 class="pb-4 text-3xl">
+            {{ page.pageTitle }}
+        </h1>
+        <p>{{ page.content }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['page'],
+}
+</script>
