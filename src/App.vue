623ed84b41fb609462afdeb8e22c64c5b7383fcd
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-05-18 11:37:11 -0700

    vue cli

--- src/App.vue
+++ src/App.vue
@@ -13,36 +13,36 @@
   </page-viewer>
 </template>
 
-<script lang="js">
-import PageViewer from './components/PageViewer.vue';
-import Navbar from './components/Navbar.vue';
+<script>
+  import PageViewer from './components/PageViewer.vue';
+  import Navbar from './components/Navbar.vue';
 
-export default {
-    components: {
-        PageViewer,
-        Navbar
+  export default  {
+      components: {
+          PageViewer,
+          Navbar
+      },
+      data() {
+        return {
+          activePage: 0,
+          pages: [
+            {
+              link: {text: 'Home', url: 'index.html'},
+              pageTitle: 'Home Page',
+              content: 'This is the home page'
+            },
+            {
+              link: {text: 'About', url: 'about.html'},
+              pageTitle: 'About Page',
+              content: 'This is the about page'
+            },
+            {
+              link: {text: 'Contact', url: 'contact.html'},
+              pageTitle: 'Contact Page',
+              content: 'This is the contact page'
+            },
+        ],
+      };
     },
-    data() {
-      return {
-        activePage: 0,
-        pages: [
-          {
-            link: {text: 'Home', url: 'index.html'},
-            pageTitle: 'Home Page',
-            content: 'This is the home page'
-          },
-          {
-            link: {text: 'About', url: 'about.html'},
-            pageTitle: 'About Page',
-            content: 'This is the about page'
-          },
-          {
-            link: {text: 'Contact', url: 'contact.html'},
-            pageTitle: 'Contact Page',
-            content: 'This is the contact page'
-          },
-      ],
-    };
-  },
-};
+  };
 </script>
